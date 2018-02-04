5be9801b46b8cd48a9575cbc081277ddadbc5421
smartape <smartapeno1@gmail.com>
2018-02-04 20:57:56 +0800

    improve

--- train.py
+++ train.py
@@ -9,11 +9,36 @@
 from utils.utils import *
 from models.vgg19_tf import VGG19
 import os
+import argparse
 
 
-os.environ['CUDA_VISIBLE_DEVICES']='0'
-content_image = load_image("images/gyeongbokgung.jpg",size=[224,224])
-style_image = load_image("images/starry-night.jpg",size=(content_image.shape[1],content_image.shape[0]))
+parser = argparse.ArgumentParser()
+parser.add_argument('--VGG19_Model_Path',type=str,default='weight/vgg19.npy',help='The model_path of VGG19 where you store.')
+parser.add_argument('--content_image',type=str,default='images/content/ucas.jpg',help='The path of content_image where you store.')
+parser.add_argument('--style_image',type=str,default='images/style/starry-night.jpg',help='The path of style_image where you store.')
+parser.add_argument('--gpu_num',type=int,default=None,help='gpu number if you need')
+args = parser.parse_args()
+
+try:
+    assert os.path.exists(args.VGG19_Model_Path)
+except:
+    print ('There is no VGG19_Model file')
+
+try:
+    assert os.path.exists(args.content_image)
+except:
+    print('There is no content_image')
+
+try:
+    assert os.path.exists(args.style_image)
+except:
+    print('There is no style_image')
+
+if args.gpu_num is not None:
+    os.environ['CUDA_VISIBLE_DEVICES'] = 'args.gpu_num'
+
+content_image = load_image(args.content_image,size=[224,224])
+style_image = load_image(args.style_image,size=(content_image.shape[1],content_image.shape[0]))
 
 
 content_image = np.reshape(content_image, ((1,) + content_image.shape))
@@ -29,7 +54,7 @@ picture_g = tf.Variable(generated_image,trainable=True,dtype=tf.float32)
 picture_c = tf.placeholder(tf.float32,shape=content_image.shape,name='content')
 picture_s = tf.placeholder(tf.float32,shape=style_image.shape,name='style')
 
-model = VGG19("weight/vgg19.npy")
+model = VGG19(args.VGG19_Model_Path)
 
 
 
@@ -68,13 +93,13 @@ a_C_5, a_S_5,a_G_5 =tf.split(out_S_5,[1,1,1],axis=0)
 J_style_layer_5 = compute_layer_style_cost(a_S_5, a_G_5)
 J_style += 0.2 * J_style_layer_5
 
-J = total_cost(J_content, J_style, 10, 40)
+J_all = total_cost(J_content, J_style, 10, 40)
 
 
 
 
 optimizer = tf.train.AdamOptimizer(2)
-train_step = optimizer.minimize(J)
+train_step = optimizer.minimize(J_all)
 
 
 
@@ -90,13 +115,14 @@ with tf.Session() as sess:
 
 
         if i % 20 == 0:
-            Jt, Jc, Js = sess.run([J, J_content, J_style],feed_dict={picture_c: content_image, picture_s: style_image})
+            J_All, J_Content, J_Style = sess.run([J_all, J_content, J_style],feed_dict={picture_c: content_image, picture_s: style_image})
             print("Iteration " + str(i) + " :")
-            print("total cost = " + str(Jt))
-            print("content cost = " + str(Jc))
-            print("style cost = " + str(Js))
-            save_image("output/" + str('w')+str(i) + ".png", undo_preprocess(np.reshape(generated_image,[224,224,3])))
+            print("total cost = " + str(J_All))
+            print("content cost = " + str(J_Content))
+            print("style cost = " + str(J_Style))
 
+    save_image("images/generated/" + str(args.content_image.split('.')[-2].split('/')[-1])+str('_')+str(args.style_image.split('.')[-2].split('/')[-1]) + ".png",
+               undo_preprocess(np.reshape(generated_image,[generated_image.shape[1],generated_image.shape[2],3])))
 
 
 
